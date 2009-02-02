002686dadfe1495c9430445ce48a9d5f33afe467
Anton Rogaynis <wildriding@gmail.com>
2009-02-02 14:47:54 -0500

    Bug 473380: Cleaning up embedding/win32 and gtk test apps, r=mfinkle

--- win32/win32_test/win32_test.cpp
+++ win32/win32_test/win32_test.cpp
@@ -1,5 +1,39 @@
-// win32_test.cpp : Defines the entry point for the application.
-//
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is mozilla.org code.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *    Anton Rogaynis <wildriding@gmail.com>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 #include "stdafx.h"
 #include "win32_test.h"
@@ -30,10 +64,10 @@ public:
     void ExitModal(nsresult result);
 };
 
-// Global Variables:
-HINSTANCE hInst;                // current instance
-TCHAR szTitle[MAX_LOADSTRING];          // The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];      // the main window class name
+// Global variables:
+HINSTANCE hInst;                      // Current instance
+TCHAR szTitle[MAX_LOADSTRING];        // The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];  // The main window class name
 HWND hMainWnd;
 set<MozView*> gViews;
 HACCEL hAccelTable;
@@ -189,14 +223,14 @@ INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK OpenLocation(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                                         HINSTANCE hPrevInstance,
-                                         LPTSTR    lpCmdLine,
-                                         int       nCmdShow)
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-     // TODO: Place code here.
+    // TODO: Place code here.
     MSG msg;
 
     // Initialize global strings
@@ -254,8 +288,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
     return (int) msg.wParam;
 }
-
-
 
 //
 //  FUNCTION: MyRegisterClass()
@@ -328,7 +360,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_PAINT  - Paint the main window
 //  WM_DESTROY  - post a quit message and return
 //
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     int wmId, wmEvent;
@@ -377,7 +408,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         if (parentView && gDoModal)
             ::EnableWindow((HWND)parentView->GetParentWindow(), TRUE);
         return DefWindowProc(hWnd, message, wParam, lParam);
-        break;
     }
     case WM_DESTROY:
         if (gViews.erase(mMozView) > 0) {
@@ -394,7 +424,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PostQuitMessage(0);
             gQuit = true;
         }
-
         break;
     case WM_SIZE:
         if (mMozView) {
